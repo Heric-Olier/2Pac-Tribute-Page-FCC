8cdb34d280c1e5e0e7e39f3a7740c28bc15b135a
Heric Olier <hericolier.code@gmail.com>
2022-08-01 11:04:08 -0500

    Improve Burguer Menu y Canvas Overlay Opacity

--- js/script.js
+++ js/script.js
@@ -2,31 +2,50 @@ let navBurguer = document.querySelector(".nav__burguer");
 let navMenu = document.querySelector(".nav__menu");
 let navMenuItems = document.querySelectorAll(".nav__menu ul li");
 let navClose = document.querySelector(".nav__close");
+let offCanvasDelay = document.querySelector(".off__canvas-overlay");
 
+//*!--------------- Burguer Menu ---------------*//
+
+// El menu se muestra y se oculta con un click en el icono de hamburguesa
 
 navBurguer.addEventListener("click", () => {
   navMenu.classList.toggle("active");
   navBurguer.classList.toggle("active");
+  offCanvasDelay.classList.toggle("active");
 });
+
+// El menú se cierra al hacer clicck en el botón de cerrar
 
 navClose.addEventListener("click", () => {
   navMenu.classList.toggle("active");
   navBurguer.classList.toggle("active");
+  offCanvasDelay.classList.toggle("active");
 });
 
+// El menú se cierra al hacer click fuera del mismo
 
-navMenuItems.forEach(item => {
+offCanvasDelay.addEventListener("click", () => {
+  navMenu.classList.toggle("active");
+  navBurguer.classList.toggle("active");
+  offCanvasDelay.classList.toggle("active");
+});
+
+// El menú se cierra al hacer click en un elemento del mismo
+
+navMenuItems.forEach((item) => {
   item.addEventListener("click", () => {
     navMenu.classList.toggle("active");
     navBurguer.classList.toggle("active");
+    offCanvasDelay.classList.toggle("active");
   });
 });
 
+//*!--------------- scroll up & Top Button ---------------*//
 
-//*! scroll up *//
 let topButton = document.querySelector(".top__button");
 let scrollUp = document.querySelector(".scroll-up");
 
+// El botón de subir se muestra y se oculta con el scroll
 
 window.addEventListener("scroll", () => {
   if (window.scrollY > 100) {
