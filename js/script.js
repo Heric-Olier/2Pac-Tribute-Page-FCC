80d1bf2733e2de11c5e100f714f5bd4a0868df9f
Heric Olier <hericolier.code@gmail.com>
2022-08-09 08:53:42 -0500

    fix Some Icons & simplify Javascript Code

--- js/script.js
+++ js/script.js
@@ -1,54 +1,46 @@
-let navBurguer = document.querySelector(".nav__burguer");
-let navMenu = document.querySelector(".nav__menu");
-let navMenuItems = document.querySelectorAll(".nav__menu ul li");
-let offCanvasDelay = document.querySelector(".off__canvas-overlay");
-let navBurguerLine1 = document.querySelector(".nav__burguer-line-1");
-let navBurguerLine2 = document.querySelector(".nav__burguer-line-2");
-let navBurguerLine3 = document.querySelector(".nav__burguer-line-3");
+const navBurguer = document.querySelector(".nav__burguer");
+const navMenu = document.querySelector(".nav__menu");
+const navMenuItems = document.querySelectorAll(".nav__menu ul li");
+const offCanvasDelay = document.querySelector(".off__canvas-overlay");
+const navBurguerLine1 = document.querySelector(".nav__burguer-line-1");
+const navBurguerLine2 = document.querySelector(".nav__burguer-line-2");
+const navBurguerLine3 = document.querySelector(".nav__burguer-line-3");
+const iconParallaxOne = document.querySelector(".icon__float-1");
+const iconParallaxTwo = document.querySelector(".icon__float-2");
+const iconParallaxThree = document.querySelector(".icon__float-3");
+const topButton = document.querySelector(".top__button");
 
 //*!--------------- Burguer Menu ---------------*//
 
-// El menu se muestra y se oculta con un click en el icono de hamburguesa
-
-navBurguer.addEventListener("click", () => {
+const showNav = () => {
   navMenu.classList.toggle("active");
   navBurguerLine1.classList.toggle("active");
   navBurguerLine2.classList.toggle("active");
   navBurguerLine3.classList.toggle("active");
   navBurguer.classList.toggle("active");
   offCanvasDelay.classList.toggle("active");
+};
+
+// El menu se muestra y se oculta con un click en el icono de hamburguesa
+navBurguer.addEventListener("click", () => {
+  showNav();
 });
 
 // El menú se cierra al hacer click fuera del mismo
-
 offCanvasDelay.addEventListener("click", () => {
-  navMenu.classList.toggle("active");
-  navBurguerLine1.classList.toggle("active");
-  navBurguerLine2.classList.toggle("active");
-  navBurguerLine3.classList.toggle("active");
-  navBurguer.classList.toggle("active");
-  offCanvasDelay.classList.toggle("active");
+  showNav();
 });
 
 // El menú se cierra al hacer click en un elemento del mismo
-
 navMenuItems.forEach((item) => {
   item.addEventListener("click", () => {
-    navMenu.classList.toggle("active");
-    navBurguerLine1.classList.toggle("active");
-    navBurguerLine2.classList.toggle("active");
-    navBurguerLine3.classList.toggle("active");
-    navBurguer.classList.toggle("active");
-    offCanvasDelay.classList.toggle("active");
+    showNav();
   });
 });
 
 //*!--------------- scroll up & Top Button ---------------*//
 
-let topButton = document.querySelector(".top__button");
-
 // El botón de subir se muestra y se oculta con el scroll
-
 window.addEventListener("scroll", () => {
   if (window.scrollY > 100) {
     topButton.classList.add("active");
@@ -57,18 +49,11 @@ window.addEventListener("scroll", () => {
   }
 });
 
-// mover objetos con el scroll
-
 //*!--------------- Parallax ---------------*//
-
-let iconParallaxOne = document.querySelector(".icon__float-1");
-let iconParallaxTwo = document.querySelector(".icon__float-2");
-let iconParallaxThree = document.querySelector(".icon__float-3");
 
 //movemos el objeto suavemente con el scroll
 window.addEventListener("scroll", () => {
   let scrollPosition = window.scrollY;
-  // iconParallaxOne.style.top = scrollPosition * -0.5 + "px";
 
   if (scrollPosition) {
     iconParallaxOne.style =
